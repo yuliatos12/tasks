5eb1d415801a47359ec266a9a94eac94d5d19994
yuliatos12 <125970340+yuliatos12@users.noreply.github.com>
2023-08-14 15:25:39 +0300

    +

--- script.js
+++ script.js
@@ -4,34 +4,64 @@ function handleClick(event) {
 const tilesEl = containerEl.querySelectorAll('.tile');
 const clickedTile = event.target.closest('.tile');  
 const clickedTileIndex = [...clickedTile.parentNode.children].indexOf(clickedTile);
-console.log(clickedTileIndex);
-// const isEmpty = tilesEl[clickedTileIndex + 1].classList.contains('empty');
-const isEmpty = clickedTileIndex + 1 < tilesEl.length ? tilesEl[clickedTileIndex + 1].classList.contains('empty') : false;
+// console.log(clickedTileIndex);
+
+
+//const isEmpty = tilesEl[clickedTileIndex + 1].classList.contains('empty');
+
+const isEmptyIndex = clickedTileIndex + 1;
+
+if(isEmptyIndex - clickedTileIndex == 1) {
+  containerEl.insertBefore(tilesEl[clickedTileIndex + 1], tilesEl[clickedTileIndex]);
+}
+
+const isEmptyIndexBackwards = clickedTileIndex - 1;
+if(isEmptyIndexBackwards - clickedTileIndex == 1) {
+  containerEl.insertBefore(tilesEl[clickedTileIndex], tilesEl[clickedTileIndex - 1]);
+}
+
+// const isEmptyIndexBelow = clickedTileIndex + 4;
+
+// const isEmpty = clickedTileIndex + 1 < tilesEl.length ? tilesEl[clickedTileIndex + 1].classList.contains('empty') : false;
 // console.log(isEmpty);
 
-if (isEmpty) {
-    containerEl.insertBefore(tilesEl[clickedTileIndex + 1], tilesEl[clickedTileIndex]);
-  }
+// const isEmptyIndex = clickedTileIndex + 1;
+// console.log(isEmptyIndex);
 
-// const isEmptyBackwards = tilesEl[clickedTileIndex - 1].classList.contains('empty');
-const isEmptyBackwards = clickedTileIndex - 1 < tilesEl.length ? tilesEl[clickedTileIndex - 1].classList.contains('empty') : false;
-// console.log(isEmptyBackwards);
 
-  if (isEmptyBackwards) {
-    containerEl.insertBefore(tilesEl[clickedTileIndex], tilesEl[clickedTileIndex - 1]);
-  }
-const isEmptyBelow = clickedTileIndex + 4 < tilesEl.length ? tilesEl[clickedTileIndex + 4].classList.contains('empty') : false;
 
-  if (isEmptyBelow) {
-    containerEl.insertBefore(tilesEl[clickedTileIndex + 4], tilesEl[clickedTileIndex]);
-  }
 
-const isEmptyAbove = clickedTileIndex - 4 < tilesEl.length ? tilesEl[clickedTileIndex - 4].classList.contains('empty') : false;
 
-  if(isEmptyAbove) {
-    containerEl.insertBefore(tilesEl[clickedTileIndex], tilesEl[clickedTileIndex - 4]);
-  }
-}
+
+
+
+
+
+
+
+// if (isEmpty) {
+//     containerEl.insertBefore(tilesEl[clickedTileIndex + 1], tilesEl[clickedTileIndex]);
+//   }
+
+// // const isEmptyBackwards = tilesEl[clickedTileIndex - 1].classList.contains('empty');
+// const isEmptyBackwards = clickedTileIndex - 1 < tilesEl.length ? tilesEl[clickedTileIndex - 1].classList.contains('empty') : false;
+// // console.log(isEmptyBackwards);
+
+//   if (isEmptyBackwards) {
+//     containerEl.insertBefore(tilesEl[clickedTileIndex], tilesEl[clickedTileIndex - 1]);
+//   }
+// const isEmptyBelow = clickedTileIndex + 4 < tilesEl.length ? tilesEl[clickedTileIndex + 4].classList.contains('empty') : false;
+
+//   if (isEmptyBelow) {
+//     containerEl.insertBefore(tilesEl[clickedTileIndex + 4], tilesEl[clickedTileIndex]);
+//   }
+
+// const isEmptyAbove = clickedTileIndex - 4 < tilesEl.length ? tilesEl[clickedTileIndex - 4].classList.contains('empty') : false;
+
+//   if(isEmptyAbove) {
+//     containerEl.insertBefore(tilesEl[clickedTileIndex], tilesEl[clickedTileIndex - 4]);
+//   }
+ }
 
 
 
