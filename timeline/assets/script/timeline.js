4b826dacbf189b62df34bdf21f55b0fc7e99ec73
yuliatos12 <125970340+yuliatos12@users.noreply.github.com>
2023-08-23 10:49:22 +0300

    +

---
+++ timeline/assets/script/timeline.js
@@ -0,0 +1,54 @@
+let timelineWidth = 0;
+let panelWidth = 0;
+let firstRun = true;
+let totalPanels = 0;
+let currentPanel = 0;
+
+$(document).ready(function () {
+    panelWidth = $('.timeline .panel').width();
+    timelineWidth = $('.timeline').width();
+    totalPanels = $('.timeline .panel').length;
+
+    adjustLayout();
+    setInterval(checkWindowSize, 1000);
+});
+
+function adjustLayout() {
+    $('.timeline .panel').each(function (index) {
+        let newX = panelWidth * index;
+        $(this).css('left', newX + 'px');
+
+        let newLabel = $(this).find('.label').html();
+        $('.timeline nav').append('<a href="#">' + newLabel + '</a>');
+    });
+
+    currentPanel = $('.timeline nav a:last-child').index();
+    activateNav();
+}
+
+function activateNav() {
+    $('.timeline nav a').on('click', function () {
+       currentPanel = $(this).index();
+        timelineWidth = $('.timeline').width();
+       $('.timeline nav a').removeClass('selected');
+       $(this).addClass('selected');
+
+       let timelineOffset = (timelineWidth - panelWidth) * .5;
+       let newPosition = ((currentPanel * panelWidth) * -1) + timelineOffset;
+
+       $('.panel_slider').animate({left: newPosition + 'px'},1000);
+    });
+}
+
+// function checkWindowSize() {
+//     let newTimelineWidth = $('.timeline').width();
+//         if( newTimelineWidth > 500 && timelineWidth < 500) {
+//             firstRun = true;
+//         }
+//     timelineWidth = newTimelineWidth;
+//
+//     if( firstRun == true){
+//         $('.timeline nav a:nth-child('+(currentPanel + 1)+')').trigger('click');
+//         firstRun = false
+//     }
+// }
